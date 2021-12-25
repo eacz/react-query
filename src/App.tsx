ba673555e97b7ddf370feea6cb58481e35230fb7
Esteban Canteros <estebanacanteros@gmail.com>
2021-12-25 18:03:12 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Posts } from './components/Posts';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Blog Posts</h1>
+    <Posts />
+  </div>
+  );
+}
+
+export default App;
