d59660ae9b41b7d30e4cb44a87261dbf20f4b1f1
Esteban Canteros <estebanacanteros@gmail.com>
2021-12-27 21:22:43 -0300

    fetching comments of posts

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,4 +1,6 @@
 import { Post } from "../interfaces";
+import { Comment } from '../interfaces/index';
+import { useQuery } from 'react-query';
 
 async function fetchComments(postId: number) {
   const response = await fetch(
@@ -28,15 +30,18 @@ interface Props {
 }
 export function PostDetail({ post } : Props) {
   // replace with useQuery
-  const data:any = [];
+  const { data, isLoading, isError } = useQuery<Comment[]>('comments', () => fetchComments(post.id));
 
   return (
     <>
+
       <h3 style={{ color: "blue" }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment:any) => (
+      {isLoading && <h3 style={{color: 'aquamarine'}}>Loading comments...</h3>}
+      {isError && <h3 style={{color: 'red'}} >Error loading comments, please refresh</h3>}
+      {data?.map((comment) => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
